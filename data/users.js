53e884ca65b115057eca9dd1b5d49d033b94f61c
Guibert <34744071@comunidadort.edu.ar>
2023-06-08 22:05:14 -0300

    Se agrega /api para el app.use; Se agrega la funcion addUser para dar de alta usuario en data, lo mismo en controllers y router;

--- data/users.js
+++ data/users.js
@@ -1,16 +1,21 @@
-const conn = require('./connection');
-const DATABASE = 'recetario';
-const USERS = 'users';
+const conn = require("./connection");
+const DATABASE = "recetario";
+const USERS = "users";
 
-
-async function getUsers(){
-    const connectiondb = await conn.getConnection();
-    const users = await connectiondb
-                        .db(DATABASE)
-                        .collection(USERS)
-                        .find()
-                        .toArray();    
-    return users;
+async function getUsers() {
+  const connectiondb = await conn.getConnection();
+  const users = await connectiondb
+    .db(DATABASE)
+    .collection(USERS)
+    .find()
+    .toArray();
+  return users;
 }
 
-module.exports = {getUsers};
+async function addUser(user) {
+  const clientMongo = await connection.getConnection();
+  const res = await clientMongo.db(DATABASE).collection(USERS).insertOne(user);
+  return res;
+}
+
+module.exports = { getUsers, addUser };
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const controller = require("../controllers/users");
 
@@ -8,5 +8,10 @@ router.get("/", async (req, res) => {
   res.json(await controller.getUsers());
 });
 
+router.post("/", async (req, res) => {
+  const user = req.body;
+  const result = await controller.addUser(user);
+  res.json(result);
+});
 
 module.exports = router;
