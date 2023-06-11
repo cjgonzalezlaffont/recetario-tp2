f37d0c305ca8cf6bd716c17696bb2304135093e0
LuLias <lucia.liascovich@gmail.com>
2023-06-11 15:50:32 -0300

    id y key en .env

--- data/recipes.js
+++ data/recipes.js
@@ -1,7 +1,9 @@
 const express = require ('express');
 const app = express();
 //const fetch = require('node-fetch').default;
-const url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDtB12vXT4bZzSI7A27bGeS_42UUkgo320&cx=172bc9b10be954c63&q=pollo%20recetas%0A';
+const ID = process.env.IDBUSCADOR;
+const KEY = process.env.KEY;
+const url = `https://www.googleapis.com/customsearch/v1?key=${KEY}&cx=${ID}&q=pollo%20recetas%0A`;
 
 
 async function getRecipes () {
